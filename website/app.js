33f964e26c6a7bf970bfb58d9810897f8c13152a
odorra <72552566+odorra@users.noreply.github.com>
2020-12-27 21:52:16 +0200

    Update app.js

--- website/app.js
+++ website/app.js
@@ -1,24 +1,20 @@
 /* Global Variables */
-const btn = document.getElementById("generate");
-const feeling = document.getElementById("feelings");
-
-const zipInput = document.getElementById("zip");
 // Create a new date instance dynamically with JS
-let d = newDate();
+let d = new Date();
 let newDate = d.getMonth() + 1 + "." + d.getDate() + "." + d.getFullYear();
-const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
-const apiKey = "&appid=40a8d93c760fd89e7f9624dda613ae1e&units=metric";
-const url= baseUrl + "?zip=" + zipInput.value + apiKey
 //Take care that getMonth() counts month from 0 to 11 so you need to add 1 to its response 
 const getData = async () => {
   //code to fetch data from api (await the fetch)
+  const zipInput = document.getElementById("zip");
+  const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
+  const apiKey = "&appid=40a8d93c760fd89e7f9624dda613ae1e&units=metric";
+  const url= baseUrl + "?zip=" + zipInput.value + apiKey
   const request = await fetch (url);
-
   try {
     //code to convert json data and return result (await conversion)
     const call = async () => {
       const response = await request.json()
-      return call()}
+      return response}
   } catch (error) {
     //code to log error
     console.log(error)
@@ -46,7 +42,7 @@ const postData = async (url ='/postData', data = {}) => {
 
 const updateUI = async () => {
   //code to fetch data from server. Use url from GET route (await the fetch)
-  await fetch('/getData');
+  const request = await fetch('/getData');
   try {
       //code to convert json data. (await conversion)
       //code to update the UI with fetched data
@@ -60,10 +56,12 @@ const updateUI = async () => {
       console.log("error", error)
   }
 };
+const btn = document.getElementById("generate");
 btn.addEventListener('click', performAction) 
 //Don't use parentheses for function inside event listener or it will not wait for event  
 function performAction() {
+  const feeling = document.getElementById("feelings");
   getData()
-      .then((data) => postData('/postData', { temp: 'data.main.temp', date: newDate, content: feeling.value }))
+      .then((data) => postData('/postData', { temp: data.main.temp, date: newDate, content: feeling.value }))
       .then(() => updateUI())       
 };
